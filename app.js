867c011db9587b36f3dabe4037fc7f436fe7e425
Himani Gautam <himanigautam2109@gmail.com>
2025-10-06 19:51:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -111,6 +111,6 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).send (message);
 });
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("working");
 });
